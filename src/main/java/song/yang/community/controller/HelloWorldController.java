c5a1112377f044529c2af6a174ade6cfbba609d6
yangsong <m17674032595@163.com>
2019-07-26 16:44:33 +0800

    init repository

---
+++ src/main/java/song/yang/community/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package song.yang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Auther: song
+ * @Date: 2019/7/26 15:50
+ * @Description:
+ */
+@Controller
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
